6a9ea5fba55ef476329517d78d8208b2b40ad008
ChocolateCharlie <aurore.amrit@gmail.com>
2017-11-11 21:36:46 +0100

    Create a new project : TopQuiz

--- OpenClassrooms/PremiereActivite/app/src/main/java/com/example/premiereactivite/MainActivity.java
+++ OpenClassrooms/PremiereActivite/app/src/main/java/com/example/premiereactivite/MainActivity.java
@@ -2,19 +2,24 @@ package com.example.premiereactivite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView monTexte = null;
+    RelativeLayout layout = null;
+    TextView text = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        monTexte = (TextView)findViewById(R.id.text);
-        monTexte.setText("Le texte de notre TextView");
-        monTexte.setPadding(50, 60, 70, 90);
+        /* Récupérer le layout par désérialisation. La méthode inflate retourne un View. */
+        layout = (RelativeLayout) RelativeLayout.inflate(this, R.layout.activity_main, null);
+
+        /* Récupérer le TextView grâce à son identifiant. */
+        text = (TextView) layout.findViewById(R.id.text);
+        text.setText("Et cette fois, ça fonctionne !");
+        setContentView(layout);
     }
 }
