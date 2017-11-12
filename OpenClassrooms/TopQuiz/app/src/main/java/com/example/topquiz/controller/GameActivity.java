79e36ec1c71da69cd9a76a87198d7a670a11cd0d
ChocolateCharlie <aurore.amrit@gmail.com>
2017-11-12 19:49:49 +0100

    Finish 'Implémentez la logique du jeu dans le contrôleur'

--- OpenClassrooms/TopQuiz/app/src/main/java/com/example/topquiz/controller/GameActivity.java
+++ OpenClassrooms/TopQuiz/app/src/main/java/com/example/topquiz/controller/GameActivity.java
@@ -2,8 +2,10 @@ package com.example.topquiz.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.topquiz.R;
 import com.example.topquiz.model.Question;
@@ -13,8 +15,9 @@ import java.util.Arrays;
 
 
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     private QuestionBank mQuestionBank;
+    private Question mCurrentQuestion;
     private TextView mQuestionText;
     private Button mAnswer1;
     private Button mAnswer2;
@@ -27,13 +30,26 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         mQuestionBank = this.generateQuestions();
+        mCurrentQuestion = mQuestionBank.getQuestion();
+
         mQuestionText = (TextView) findViewById(R.id.activity_game_question_text);
         mAnswer1 = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswer1.setOnClickListener(this);
+        mAnswer1.setTag(0);
         mAnswer2 = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswer2.setOnClickListener(this);
+        mAnswer1.setTag(1);
         mAnswer3 = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswer3.setOnClickListener(this);
+        mAnswer1.setTag(2);
         mAnswer4 = (Button) findViewById(R.id.activity_game_answer4_btn);
+        mAnswer4.setOnClickListener(this);
+        mAnswer1.setTag(3);
+
+        this.displayQuestion(mCurrentQuestion);
     }
 
+/* Initialise questions and return a questionbank */
     protected QuestionBank generateQuestions() {
         // Create questions
         Question question1 = new Question("Who is the creator of Android ?",
@@ -48,5 +64,27 @@ public class GameActivity extends AppCompatActivity {
 
         // Return QuestionBank
         return new QuestionBank(Arrays.asList(question1, question2, question3));
+    }
+
+/* Replace default text by question text */
+    private void displayQuestion(final Question question) {
+        mQuestionText.setText(question.getQuestion());
+        mAnswer1.setText(question.getChoicelist().get(0));
+        mAnswer2.setText(question.getChoicelist().get(1));
+        mAnswer3.setText(question.getChoicelist().get(2));
+        mAnswer4.setText(question.getChoicelist().get(3));
+    }
+
+/* Check user answer and show toast depending on correctness */
+    @Override
+    public void onClick(View v) {
+        int responseIndex = (int) v.getTag();
+
+        if (responseIndex == mCurrentQuestion.getAnswerIndex()) {
+            Toast.makeText(this, "Correct !", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Nice try !", Toast.LENGTH_SHORT).show();
+        }
     }
 }
